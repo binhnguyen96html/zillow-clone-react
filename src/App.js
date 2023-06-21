310de1aa32ce6f23ed1dfa0a4fd03ccf17bc2210
Binh <binhnguyendxbp@gmail.com>
2023-06-21 00:51:38 -0700

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
     <>
       <h1 className="text-2xl">Hello world</h1>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,9 @@
+
+
+export default function SignIn() {
+  return (
+    <div>
+      Sign in
+    </div>
+  )
+}
