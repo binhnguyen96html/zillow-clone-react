9a7524db428336ab65d32bdc856e66ec647870d0
Binh <binhnguyendxbp@gmail.com>
2023-07-06 19:59:06 -0700

    add share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,70 +1,85 @@
 import { doc, getDoc } from "firebase/firestore";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { db } from "../firebase";
 import Spinner from "../components/Spinner";
-import {Swiper, SwiperSlide} from 'swiper/react';
-import {
-    EffectFade,
-    Autoplay,
-    Navigation,
-    Pagination,
-  } from "swiper/modules";
-  import SwiperCore from 'swiper';
-import 'swiper/css/bundle';
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
+import SwiperCore from "swiper";
+import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopy, setShareLinkCopy] = useState(false);
 
-    const params = useParams();
-    const [listing, setListing] = useState(null);
-    const [loading, setLoading] = useState(true);
+  SwiperCore.use([Autoplay, Navigation, Pagination]);
 
-    SwiperCore.use([Autoplay, Navigation, Pagination]);
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    };
 
-    useEffect(() => {
-        const fetchListing = async () =>{
-            const docRef = 
-            doc(db, 'listings', params.listingId);
-            const docSnap = await getDoc(docRef);
-            if(docSnap.exists()){
-                setListing(docSnap.data());
-                setLoading(false);
-            }
-        }   
+    fetchListing();
+  }, [params.listingId]);
 
-        fetchListing();
-    },[params.listingId])
-
-
-    if(loading){
-        return <Spinner />
-    }
+  if (loading) {
+    return <Spinner />;
+  }
 
   return (
     <main>
-      <Swiper 
-      slidesPerView={1}
-      navigation
-      pagination={{type:'progressbar'}}
-      effect="fade"
-      modules={EffectFade}
-      autoplay={{delay:3000}}
+      <Swiper
+        slidesPerView={1}
+        navigation
+        pagination={{ type: "progressbar" }}
+        effect="fade"
+        modules={EffectFade}
+        autoplay={{ delay: 3000 }}
       >
-        {listing.imgUrls.map((url, index)=>(
-            <SwiperSlide key={index}>
-                <div 
-                className="w-full overflow-hidden h-[300px]"
-                style={{background: `url(${listing.imgUrls[index]})
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="w-full overflow-hidden h-[300px]"
+              style={{
+                background: `url(${listing.imgUrls[index]})
                 center no-repeat`,
-                backgroundSize:'cover',
-            }}
-                >
-
-                </div>
-            </SwiperSlide>
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </SwiperSlide>
         ))}
       </Swiper>
+
+      {/* COPY FUNCTION  */}
+      <div
+        className="fixed top-[13%] right-[3%]
+        z-10 bg-white cursor-pointer
+        border-2 border-gray-400 rounded-full
+        w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopy(true);
+          setTimeout(() => {
+            setShareLinkCopy(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkCopy && 
+      <p
+      className="fixed top-[20%] right-[5%] font-semibold
+      border-2 border-gray-400 rounded-md bg-white z-10
+      p-2"
+      >Link Copied</p>}
     </main>
-  )
+  );
 }
